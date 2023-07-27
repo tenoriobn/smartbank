331764928fed0649b11083e5fe6fda2509e55d85
Bruno Tenório <tenoriobn@gmail.com>
2023-07-27 15:06:38 -0300

    Utilizando media query no styled components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -16,6 +16,10 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `;
 
 export default () => {
